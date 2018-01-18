e2ae63ace3e9f986681d11d0a886c0ce9a1b2284
Harshdeep Bilaiya <hbilaiya2@gmail.com>
2018-01-18 21:23:41 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  username:string = "";
+  isEmpty:boolean = true;
   title = 'app';
+  formData: string = 'Harshita';
+  isActive: boolean = false;
+  fruits: any = ['apple', 'banana', 'orange'];
+  getFormData(input: any): void {
+    // console.log(input);
+    this.formData = (<HTMLInputElement>input.target).value;
+  }
+  onSubmit():void {
+  
+    this.username = '';
+  }
+  constructor() {
+    // this.formData = 'Harshita';
+    // console.log(this.formData);
+  }
+
+  toggleList(): void {
+    this.isActive = !this.isActive;
+  }
 }
