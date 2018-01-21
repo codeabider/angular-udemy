36c8b162a323acc1115089a0457421c1916e222e
Harshita Maheshwari <harshitamaheshwari93@gmail.com>
2018-01-21 22:10:30 +0530

    assignments 2-3

---
+++ assignments2-3/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // assignment 2
+  // title = 'app';
+  // isDisabled:boolean = false;
+  constructor() {
+        
+  }
+
+  // assignment 3
+  // showDetails:boolean = false;
+  // buttons:any= [];
+  // toggleDetails(){
+  //   let date: Date = new Date();
+  //   this.showDetails = !this.showDetails;
+  //   this.buttons.push(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);     
+  // }
+}
