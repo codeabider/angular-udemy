527f7a2c86df25a689c8de236f9ee2c3652e8774
Harshita Maheshwari <harshitamaheshwari93@gmail.com>
2018-01-27 01:17:47 +0530

    Lecture 50 : Display Recipe Details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipes.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe("Laddoo", "Laddoos are made of boondi","https://recipes.timesofindia.com/photo/55048059.cms")
+  ];
   constructor() { }
 
   ngOnInit() {
+
   }
+  
 
 }
